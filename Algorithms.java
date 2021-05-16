2794541d9027158036b2365c592a6ecb763742fb
Mahmut <leipzig9998@hotmail.com>
2021-05-16 17:50:02 +0300

    quick sort with first element pivot added,1

--- Algorithms.java
+++ Algorithms.java
@@ -70,57 +70,37 @@ public class Algorithms {
         }
     }
 
-    static int partition(int array[], int low, int high) {
+    
 
-        // choose the rightmost element as pivot
-        int pivot = array[high];
-
-        // pointer for greater element
-        int i = (low - 1);
-
-        // traverse through all elements
-        // compare each element with pivot
-        for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
-
-                // if element smaller than pivot is found
-                // swap it with the greatr element pointed by i
-                i++;
-
-                // swapping element at i with element at j
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-
-        }
-
-        // swapt the pivot element with the greater element specified by i
-        int temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-
-        // return the position from where partition is done
-        return (i + 1);
-    }
-
-    public static void quickSort(int array[], int low, int high) {
-        if (low < high) {
-
-            // find pivot element such that
-            // elements smaller than pivot are on the left
-            // elements greater than pivot are on the right
-            int pi = partition(array, low, high);
-
-            // recursive call on the left of pivot
-            quickSort(array, low, pi - 1);
-
-            // recursive call on the right of pivot
-            quickSort(array, pi + 1, high);
-        }
-    }
+    
 
     public void quickSortV2() {
+    }
+    public static int partition(int[] a, int left, int right) {
+        int pivot = a[left];
+        while(left<=right) {
+            while(a[left] < pivot)
+                left++;
+            while(a[right] > pivot)
+                right--;
+            if(left<=right) {
+                int tmp = a[left];
+                a[left] = a[right];
+                a[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+    public static void recursiveQuickSort(int[] a, int i, int j) {
+        int idx = partition(a, i, j);
+        if(i < idx-1) {
+            recursiveQuickSort(a, i, idx-1);
+        }
+        if(j > idx) {
+            recursiveQuickSort(a, idx, j);
+        }
     }
 
     // For heap sort
--- test.java
+++ test.java
@@ -29,7 +29,8 @@ public class test {
             // Algorithms.quickSort(input.getArr(), 0, input.getArr().length-1);
             // Algorithms.mergeSort(input.getArr(), 0, input.getArr().length - 1);
             // Algorithms.heapSort(input.getArr()); 
-             Algorithms.countingsort(input.getArr(), input.getArr().length);
+             //Algorithms.countingsort(input.getArr(), input.getArr().length);
+             Algorithms.recursiveQuickSort(input.getArr(), 0, input.getArr().length-1);
             long end = System.nanoTime();
             long elapsedTime = end - start;
 
